5fa1d0d7aa0e2250237b08cdb322e362491c6bb6
Akilan K <akilankdev@gmail.com>
2026-08-27 11:31:35 +0530

    describe() to group test cases and used 'it' instead of 'test'

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,25 +1,27 @@
-import { render,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
-import {toBeInTheDocument} from "@testing-library/jest-dom";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
 
-/* test("Should render contact component into DOM",() => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
-});
-*/
-//would pass.
-test("Should Submit text exist in contact component",() => {
-  render(<Contact />);
-  const button = screen.getByText("Submit");
-  expect(button).toBeInTheDocument();
-}); 
-//would pass
-test("Should 2 input boxes exist in contact component",() => {
-  render(<Contact />);
-  //Quering
-  const inputBoxes = screen.getAllByRole("textbox");
-  console.log(inputBoxes); //Displays the Array of two objects.Those objs are basically the virtual dom.
-  //assertion
-  expect(inputBoxes.length).toBe(2);
+//describe() is used to group test cases.
+
+describe("Contact us test cases", () => {
+  //We can also use 'it' keyword instead of 'test',both are the same and works the same.
+  it("Should render contact component into DOM", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("Should Submit text exist in contact component", () => {
+    render(<Contact />);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
+
+  it("Should 2 input boxes exist in contact component", () => {
+    render(<Contact />);
+    //Quering
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(2);
+  });
 });
